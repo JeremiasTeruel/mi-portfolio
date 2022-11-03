dcfed85318bfc4ed08f37aacc8b4294379f6526b
Teje8 <jteruel8@gmail.com>
2022-11-03 20:46:42 -0300

    change component and style

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,117 @@
 import React from 'react'
 import './Services.css'
+import {BsCheck} from 'react-icons/bs'
 
 const Services = () => {
     return (
-        <div>Services</div>
+        <section id='services'>
+            <h2>Mis servicios</h2>
+
+            <div className='container services_container'>
+                <article className='service'>
+                    <div className='service_head'>
+                        <h3>Diseño y Desarrollo Web</h3>
+                    </div>
+
+                    <ul className='service_list'>
+                        <li>
+                            <BsCheck className='service_list-icon'/>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BsCheck className='service_list-icon'/>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BsCheck className='service_list-icon'/>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BsCheck className='service_list-icon'/>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BsCheck className='service_list-icon'/>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BsCheck className='service_list-icon'/>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </li>
+                    </ul>
+                </article>
+
+                {/* END OF DISEÑO */}
+
+                <article className='service'>
+                    <div className='service_head'>
+                        <h3>Diseño y Desarrollo Web</h3>
+                    </div>
+
+                    <ul className='service_list'>
+                        <li>
+                            <BsCheck className='service_list-icon'/>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BsCheck className='service_list-icon'/>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BsCheck className='service_list-icon'/>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BsCheck className='service_list-icon'/>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BsCheck className='service_list-icon'/>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BsCheck className='service_list-icon'/>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </li>
+                    </ul>
+                </article>
+
+                {/* END OF DISEÑO */}
+
+                <article className='service'>
+                    <div className='service_head'>
+                        <h3>Diseño y Desarrollo Web</h3>
+                    </div>
+
+                    <ul className='service_list'>
+                        <li>
+                            <BsCheck className='service_list-icon'/>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BsCheck className='service_list-icon'/>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BsCheck className='service_list-icon'/>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BsCheck className='service_list-icon'/>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BsCheck className='service_list-icon'/>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <BsCheck className='service_list-icon'/>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        </li>
+                    </ul>
+                </article>
+            </div>
+        </section>
     )
 }
 
