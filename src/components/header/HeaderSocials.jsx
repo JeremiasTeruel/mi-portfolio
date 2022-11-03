cefcd8b467394f16d099ce75771a81362bd60c6c
Teje8 <jteruel8@gmail.com>
2022-11-03 20:46:11 -0300

    change component and style

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
 import {FaGithub} from 'react-icons/fa'
+import {TfiInstagram} from 'react-icons/tfi'
+import {FaFacebookF} from 'react-icons/fa'
+
 
 const HeaderSocials = () => {
     return (
         <div className='header_socials'>
             <a href='https://www.linkedin.com/in/jeremias-teruel/'><BsLinkedin/></a>
             <a href='https://github.com/JeremiasTeruel'><FaGithub/></a>
+            <a href='https://www.instagram.com/terueljjj/'><TfiInstagram/></a>
+            <a href='https://www.facebook.com/jere.teruel/'><FaFacebookF/></a>
         </div>
     )
 }
