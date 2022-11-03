8ca1c316a8666226ccd32d35ef4f26d4948932f8
Teje8 <jteruel8@gmail.com>
2022-11-03 20:44:46 -0300

    change component and style

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
 import './About.css'
+import ME from '../../assets/yo2.JPG'
+import {AiOutlineFundProjectionScreen} from 'react-icons/ai'
+import {AiOutlineProject} from 'react-icons/ai'
+import {TbAward} from 'react-icons/tb'
 
 const About = () => {
     return (
-        <div>About</div>
+        <section id='about'>
+            <h2>Sobre mi</h2>
+
+            <div className='container about_container'>
+                <div className='about_me'>
+                    <div className="about_me-image">
+                        <img src={ME} alt="me" />
+                    </div>
+                </div>
+                <div className='about_content'>
+                    <div className="about_cards">
+                        <article className='about_card'>
+                            <AiOutlineFundProjectionScreen className='about_icon'/>
+                            <h5>Experiencia</h5>
+                            <small>+700hs de experiencia FrontEnd y BackEnd</small>
+                        </article>
+                        <article className='about_card'>
+                            <AiOutlineProject className='about_icon'/>
+                            <h5>Proyectos</h5>
+                            <small>+5 proyectos individuales y grupales</small>
+                        </article>
+                        <article className='about_card'>
+                            <TbAward className='about_icon'/>
+                            <h5>Educación</h5>
+                            <small>Egresado Bootcamp Full Stack MERN de MindHub</small>
+                        </article>
+                    </div>
+
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius tempora, similique voluptate facilis, asperiores laudantium corporis omnis earum saepe quis vel ullam sint doloremque? Laborum eaque dolore maiores non adipisci.</p>
+                </div>
+            </div>
+        </section>
     )
 }
 
