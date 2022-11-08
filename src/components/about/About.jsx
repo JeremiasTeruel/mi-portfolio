1d9cb3124ed2502b75a16097615919299d3072cc
Teje8 <jteruel8@gmail.com>
2022-11-08 16:59:45 -0300

    modify about text add img

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,6 +4,7 @@ import ME from '../../assets/yo2.JPG'
 import {AiOutlineFundProjectionScreen} from 'react-icons/ai'
 import {AiOutlineProject} from 'react-icons/ai'
 import {TbAward} from 'react-icons/tb'
+import insignia from '../../assets/insigniaMindHub.png'
 
 const About = () => {
     return (
@@ -35,7 +36,10 @@ const About = () => {
                         </article>
                     </div>
 
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius tempora, similique voluptate facilis, asperiores laudantium corporis omnis earum saepe quis vel ullam sint doloremque? Laborum eaque dolore maiores non adipisci.</p>
+                    <div className='insignia'>
+                        <p>Programador junior Full Stack MERN, egresado del bootcamp de MindHub de +700hs de entrenamiento y simulación de ambiente laboral, utilizando metodologias agiles, scrum y "learning by doing", y tecnologias como HTML, CSS, JavaScript, React Js, React Native, Node Js, Express, Mongo DB, API REST, Nodemailer, Joi, Nodemon, Mongoose.</p>
+                        <img src={insignia} alt="logo" className='insignia_img'/>
+                    </div>
                 </div>
             </div>
         </section>
