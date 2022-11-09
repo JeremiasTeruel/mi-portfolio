eb1af2b366513c2519c9dc76abfd61a65234ad7d
Teje8 <jteruel8@gmail.com>
2022-11-09 18:49:39 -0300

    fix root render

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import ReactDOM from "react-dom";
+import * as ReactDOMClient from "react-dom/client";
 import App from './App'
 import './index.css'
 
-ReactDOM.render(<App/>, document.querySelector('#root'))
+const rootElement = document.querySelector('#root')
+const root = ReactDOMClient.createRoot(rootElement)
+root.render(<App/>)
